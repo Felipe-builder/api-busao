2d73dd8fcedb205ec6d31c1adb92f877469253d3
Felipe-builder <felipesvascon@gmail.com>
2022-01-05 11:40:08 -0300

    Protegendo dados do usuario com filtragem de dados sensíveis

--- api/rotas/usuarios/TabelaUsuario.js
+++ api/rotas/usuarios/TabelaUsuario.js
@@ -3,7 +3,7 @@ const Modelo = require('./ModeloTabelaUsuario')
 
 module.exports = {
     listar() {
-        return Modelo.findAll()
+        return Modelo.findAll({ raw: true })
     },
     inserir(usuario) {
         return Modelo.create(usuario)
